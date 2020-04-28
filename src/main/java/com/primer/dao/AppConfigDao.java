ac69ba4125748d3a9c6dda602fa79800b1e01aa6
primerxiao <primerxiao@163.com>
2020-04-28 12:51:07 +0800

    删除无用代码

--- src/main/java/com/primer/dao/AppConfigDao.java
+++ src/main/java/com/primer/dao/AppConfigDao.java
@@ -1,7 +1,6 @@
 package com.primer.dao;
 
 import com.primer.entity.AppConfig;
-import com.primer.entity.ApplicationConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
